450d637f15dcd0537a5e1e04473d69bd4a4b2116
honpey <houpengyang0901@126.com>
2016-06-12 08:25:40 +0800

    w

--- 0602.sh
+++ 0602.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 var1="hello world"
 var2="nihao"
+var3=1
 
 func() {
 	echo "hello this is my first function"
 	echo $var1
 }
+
+if [[ "$var1" == "hello world" ]] 
+then
+	echo "cool, it is hello world"
+fi
+
+if [[ "$var3" == "1" ]]
+then 
+	echo "var3 is 1"
+fi
 
 func
